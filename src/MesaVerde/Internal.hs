00059885ddfd7e66eaba10b373c0f3b2401d324d
Matt Parsons <parsonsmatt@gmail.com>
2018-04-13 15:30:56 -0600

    Altering the desired syntax
    In Ruby, if you can get it to parse, you can give it the semantics you
    want. Unfortunately, this is Haskell, and we have to wiggle our way
    around  GHC's stricter rules. Fortunately, we end up with a quite nice
    syntax.

--- src/MesaVerde/Internal.hs
+++ src/MesaVerde/Internal.hs
@@ -1,22 +1,76 @@
+{-# LANGUAGE AllowAmbiguousTypes        #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures             #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeApplications           #-}
 
 module MesaVerde.Internal where
 
-import Database.Persist.Types (EntityDef(..))
+import           Data.String            (IsString (..))
+import           Database.Persist.Types (EntityDef (..))
+import           GHC.Exts               (Constraint)
+import           Language.Haskell.TH    (Name)
 
-newtype EntityDefine i a = EntityDefine (IO a)
+newtype EntityDefine a = EntityDefine (IO a)
     deriving (Functor, Applicative, Monad)
 
-newtype SchemaDefine a = SchemaDefine (IO a)
+newtype SchemaDefiner a = SchemaDefiner (IO a)
     deriving (Functor, Applicative, Monad)
 
-example :: SchemaDefine a
+type SchemaDefine = SchemaDefiner ()
+
+newtype FieldLabel i = FieldLabel String
+
+instance IsString (FieldLabel i) where
+    fromString = FieldLabel
+
+example :: SchemaDefine
 example = do
-    model "User" $ do
-        "name" @String
-        "age" @Int
-        derive [''Eq, ''Ord, ''Show]
+    userRef <- model "User" $ do
+        "name"   ''String
+        "age"    ''Int
 
-    model "Dog" $ do
-        "name" @String
-        "owner" @UserId
+        derive @[Eq, Ord, Show]
+
+    let theUsual = derive @[Eq, Ord, Show]
+
+    model_ "Dog" $ do
+        "owner"  userRef
+        "name"   ''String
+
+        theUsual
+
+
+data EntityRef
+
+model :: String -> EntityDefine a -> SchemaDefiner EntityRef
+model = undefined
+
+model_ :: String -> EntityDefine a -> SchemaDefiner ()
+model_ = undefined
+
+field :: forall fieldType a. FieldLabel fieldType -> EntityDefine a
+field = undefined
+
+fldname :: String -> Name -> EntityDefine ()
+fldname = undefined
+
+fldref :: String -> EntityRef -> EntityDefine ()
+fldref = undefined
+
+derive :: forall (xs :: [* -> Constraint]) a. EntityDefine a
+derive = undefined
+
+fieldRef :: EntityRef -> String -> EntityDefine a
+fieldRef = undefined
+
+instance (() ~ a) => IsString (Name -> EntityDefine a) where
+    fromString = fldname
+
+instance (() ~ a) => IsString (EntityRef -> EntityDefine a) where
+    fromString = fldref
