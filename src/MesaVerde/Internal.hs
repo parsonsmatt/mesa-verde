1732fb2bf98cc103b61ab807e0ea15a57325fa82
Matt Parsons <parsonsmatt@gmail.com>
2018-04-13 16:51:08 -0600

    reorganization

--- src/MesaVerde/Internal.hs
+++ src/MesaVerde/Internal.hs
@@ -16,20 +16,8 @@ import           Database.Persist.Types (EntityDef (..))
 import           GHC.Exts               (Constraint)
 import           Language.Haskell.TH    (Name)
 
-newtype EntityDefine a = EntityDefine (IO a)
-    deriving (Functor, Applicative, Monad)
-
-newtype SchemaDefiner a = SchemaDefiner (IO a)
-    deriving (Functor, Applicative, Monad)
-
-type SchemaDefine = SchemaDefiner ()
-
-newtype FieldLabel i = FieldLabel String
-
-instance IsString (FieldLabel i) where
-    fromString = FieldLabel
-
-example :: SchemaDefine
+-- the desired syntax, blessed may it be
+example :: Schema
 example = do
     userRef <- model "User" $ do
         "name"   ''String
@@ -45,32 +33,75 @@ example = do
 
         theUsual
 
+    model_ "Friend" $ do
+        "name"   ''String sql { colName = "friend_name" }
+        "foobar" ''Int    sql { sqlType = "why" }
+        "ugh"    ''Int    sql { colName = "no_u", sqlType = "why" }
+
+        theUsual
+
+-- the end result of running the schema definitions will be a @['EntityDef']@.
+newtype SchemaDefine a = SchemaDefine (IO a)
+    deriving (Functor, Applicative, Monad)
+
+type Schema = SchemaDefine ()
+
+-- introduce a model
+model :: String -> EntityDefine a -> SchemaDefine EntityRef
+model = undefined
+
+-- silence the @-funused-do-bind@ or whatever
+model_ :: String -> EntityDefine a -> SchemaDefine ()
+model_ = undefined
+
+--------------------------------------------------------------------------------
+-- entity definition
+--------------------------------------------------------------------------------
+
+-- the end result of running an 'EntityDefine' will be a single 'EntityDef'.
+newtype EntityDefine a = EntityDefine (IO a)
+    deriving (Functor, Applicative, Monad)
+
+data FieldSpec = FieldSpec
+    { sqlType :: OptionalField
+    , colName :: OptionalField
+    }
+
+newtype OptionalField = OptionalField { unOptionalField :: Maybe String }
+
+instance IsString OptionalField where
+    fromString = OptionalField . Just
+
+sql :: FieldSpec
+sql = FieldSpec
+    { sqlType = OptionalField Nothing
+    , colName = OptionalField Nothing
+    }
 
 data EntityRef
 
-model :: String -> EntityDefine a -> SchemaDefiner EntityRef
-model = undefined
-
-model_ :: String -> EntityDefine a -> SchemaDefiner ()
-model_ = undefined
-
-field :: forall fieldType a. FieldLabel fieldType -> EntityDefine a
-field = undefined
-
-fldname :: String -> Name -> EntityDefine ()
-fldname = undefined
-
-fldref :: String -> EntityRef -> EntityDefine ()
-fldref = undefined
-
+-- god, this is really awful, isn't it?
 derive :: forall (xs :: [* -> Constraint]) a. EntityDefine a
 derive = undefined
 
-fieldRef :: EntityRef -> String -> EntityDefine a
-fieldRef = undefined
+--------------------------------------------------------------------------------
+-- nasty syntax hacks
+--------------------------------------------------------------------------------
 
 instance (() ~ a) => IsString (Name -> EntityDefine a) where
-    fromString = fldname
+    fromString = fieldWithType
+
+fieldWithType :: String -> Name -> EntityDefine ()
+fieldWithType = undefined
 
 instance (() ~ a) => IsString (EntityRef -> EntityDefine a) where
-    fromString = fldref
+    fromString = fieldWithRef
+
+fieldWithRef :: String -> EntityRef -> EntityDefine ()
+fieldWithRef = undefined
+
+instance (() ~ a) => IsString (EntityRef -> FieldSpec -> EntityDefine a) where
+    fromString = undefined
+
+instance (() ~ a) => IsString (Name -> FieldSpec -> EntityDefine a) where
+    fromString = undefined
