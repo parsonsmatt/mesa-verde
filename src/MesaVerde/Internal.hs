96bb33975231f386fd20c14f06d034760e7dda66
Matt Parsons <parsonsmatt@gmail.com>
2018-04-13 15:01:18 -0600

    Dummy types
    Since we're creating a DSL, and we'll likely want a monadic syntax, I'm
    going to go ahead and create some types. These types will serve as the
    scaffold for the example syntax I will define.

--- src/MesaVerde/Internal.hs
+++ src/MesaVerde/Internal.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module MesaVerde.Internal where
 
+import Database.Persist.Types (EntityDef(..))
 
+newtype EntityDefine i a = EntityDefine (IO a)
+    deriving (Functor, Applicative, Monad)
+
+newtype SchemaDefine a = SchemaDefine (IO a)
+    deriving (Functor, Applicative, Monad)
